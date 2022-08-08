03ca799bcba849ace1fd9843e60df2cd518f0e65
Kuldeep <kuldeepbishnoi.2001@gmail.com>
2022-08-08 13:01:53 +0530

    Create search.py
    search in BST

---
+++ BST/search.py
@@ -0,0 +1,61 @@
+# Question: https://practice.geeksforgeeks.org/problems/search-a-node-in-bst/1
+# References: https://www.youtube.com/watch?v=9TJYWh0adfk&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=46&ab_channel=takeUforward
+
+class BST:
+    
+    #Function to search a node in BST.
+    def search(self, node, x):
+        while (node != None and node.data != x):
+            if x<node.data:
+                node = node.left
+            else:
+                node = node.right
+        return node
+      
+# Driver Code Starts
+class Node:
+    """ Class Node """
+    def __init__(self, value):
+        self.left = None
+        self.data = value
+        self.right = None
+
+class Tree:
+    def createNode(self, data):
+        return Node(data)
+    
+    def insert(self, node, data):
+        if node is None:
+            return self.createNode(data)
+        else:
+            if data < node.data:
+                node.left = self.insert(node.left, data)
+            else:
+                node.right = self.insert(node.right, data)
+            return node
+
+    def traverseInorder(self, root):
+        if root is not None:
+            print(root.data, end= " ")
+            self.traverseInorder(root.left)
+            self.traverseInorder(root.right)
+    
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        arr = input().strip().split()
+        root = None
+        tree = Tree()
+        root = tree.insert(root, int(arr[0]))
+        for j in range(1, n):
+            root = tree.insert(root, int(arr[j]))
+        #tree.traverseInorder(root)
+        num = int(input())
+        find = BST()
+        if find.search(root, num):
+            print(1)
+        else:
+            print(0)
+
+#Driver Code Ends
